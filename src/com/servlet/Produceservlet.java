a94e9a96982b27001e2bbc300745bd7690267ded
cuilijia <602465981@qq.com>
2017-09-16 02:22:34 +0800

    1

---
+++ src/com/servlet/Produceservlet.java
@@ -0,0 +1,22 @@
+package com.servlet;
+import com.google.gson.JsonObject;
+import com.beans.produce;
+import com.dao.*;
+import com.servlet.tool.JsonUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.Writer;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "ProduceServlet")
+public class Produceservlet {
+
+
+}
