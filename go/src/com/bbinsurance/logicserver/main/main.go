0af1827191c0a6196025602fce75b8f58043e006
jiaminchen <jmchen.ggm@gmail.com>
2017-11-05 19:21:08 +0800

    加一些log

--- go/src/com/bbinsurance/logicserver/main/main.go
+++ go/src/com/bbinsurance/logicserver/main/main.go
@@ -18,26 +18,10 @@ func main() {
 	//初始化DB
 	database.InitDB()
 
+	log.Info("handle /create/article/")
 	//初始Http路径监控
-	http.Handle("/static/", http.FileServer(http.Dir("./")))
 	http.HandleFunc("/create/article/", web.HandleCreateArticle)
+
+	log.Info("listen 8081 port")
 	http.ListenAndServe(":8081", nil)
-
-	// database.InsertArticle("真心不错", "不错不错", "http://", "http://")
-	// var articleList *list.List = database.GetAllArticle()
-	// var articleSlince = make([]database.Article, articleList.Len())
-
-	// for e := articleList.Front(); e != nil; e = e.Next() {
-	// 	article := e.Value.(database.Article)
-	// 	articleSlince = append(articleSlince, article)
-	// }
-
-	// listData, _ := json.Marshal(articleSlince)
-	// fmt.Println(string(listData))
-
-	// for e := articleList.Front(); e != nil; e = e.Next() {
-	// 	article := e.Value.(database.Article)
-	// 	data, _ := json.Marshal(article)
-	// 	fmt.Println(string(data))
-	// }
 }
