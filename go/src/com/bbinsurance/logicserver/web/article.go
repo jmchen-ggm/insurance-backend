9f31b007fc3c269bbca33cd1e5d3020c6ba59ef6
jiaminchen <jmchen.ggm@gmail.com>
2017-11-10 01:28:33 +0800

    修复编译错误

--- go/src/com/bbinsurance/logicserver/web/article.go
+++ go/src/com/bbinsurance/logicserver/web/article.go
@@ -11,7 +11,7 @@ func GetListArticle(bbReq protocol.BBReq) []byte {
 	var listArticleRequest protocol.BBListArticleRequest
 	json.Unmarshal(bbReq.Body, &listArticleRequest)
 	articleList := database.GetListArticle(listArticleRequest.StartIndex, listArticleRequest.PageSize)
-	log.info("req %d %d \n %s", listArticleRequest.StartIndex, listArticleRequest.PageSize, articleList)
+	log.Info("req %d %d \n %s", listArticleRequest.StartIndex, listArticleRequest.PageSize, articleList)
 	var response protocol.BBListArticleResponse
 	response.ArticleList = articleList
 	responseBytes, _ := json.Marshal(response)
