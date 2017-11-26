a6bb1b174e908ae0e958a998f7152b1796c669ef
jiaminchen <jmchen.ggm@gmail.com>
2017-11-26 17:32:28 +0800

    调整协议目录结构

--- go/src/com/bbinsurance/userserver/web/dispatcher.go
+++ go/src/com/bbinsurance/userserver/web/dispatcher.go
@@ -2,12 +2,11 @@ package web
 
 import (
 	"com/bbinsurance/log"
-	"com/bbinsurance/userserver/protocol"
 	"com/bbinsurance/webcommon"
 	"net/http"
 )
 
-type HandleMethod func(protocol.BBReq) ([]byte, int, string)
+type HandleMethod func(webcommon.BBReq) ([]byte, int, string)
 
 var dandlerObjMap map[int]HandleMethod
 
@@ -18,20 +17,20 @@ func FunInitDataBin() {
 func FunHandleDataBin(writer http.ResponseWriter, request *http.Request) {
 	log.Info("New Request: %s %s", request.URL, request.Method)
 	bbReq, code, msg := webcommon.HandleRequest(request)
-	if code != protocol.ResponseCodeSuccess {
+	if code != webcommon.ResponseCodeSuccess {
 		webcommon.HandleErrorResponse(writer, bbReq, code, msg)
 	} else {
 		method, ok := dandlerObjMap[bbReq.Bin.FunId]
 		if ok {
 			log.Info("HandleDataBin %d", bbReq.Bin.FunId)
 			responseBytes, code, errMsg := method(bbReq)
-			if code == protocol.ResponseCodeSuccess {
+			if code == webcommon.ResponseCodeSuccess {
 				webcommon.HandleSuccessResponse(writer, bbReq, responseBytes)
 			} else {
 				webcommon.HandleErrorResponse(writer, bbReq, code, errMsg)
 			}
 		} else {
-			webcommon.HandleErrorResponse(writer, bbReq, protocol.ResponseCodeInvalidFunId, "Invalid FunId")
+			webcommon.HandleErrorResponse(writer, bbReq, webcommon.ResponseCodeInvalidFunId, "Invalid FunId")
 		}
 	}
 }
