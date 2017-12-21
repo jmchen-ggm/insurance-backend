63ef0a150e005e4767b82cf92060fa06a596d121
jiaminchen <jmchen.ggm@gmail.com>
2017-12-22 00:04:13 +0800

    fix bug

--- go/src/com/bbinsurance/homeserver/main/main.go
+++ go/src/com/bbinsurance/homeserver/main/main.go
@@ -13,6 +13,8 @@ func main() {
 	// 初始化变量
 	constants.InitConstants()
 	//初始Http路径监控
+	log.Info("static folder %s", constants.STATIC_FOLDER)
 	http.Handle("/", http.FileServer(http.Dir(constants.STATIC_FOLDER)))
+	log.Info("listen %s port", constants.PORT)
 	http.ListenAndServe(":"+constants.PORT, nil)
 }
