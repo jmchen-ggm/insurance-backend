2844272bbaffbbef69a61273dddea45d184f5c54
jiaminchen <jmchen.ggm@gmail.com>
2017-12-22 00:01:50 +0800

    fix bug

--- go/src/com/bbinsurance/homeserver/main/main.go
+++ go/src/com/bbinsurance/homeserver/main/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"com/bbinsurance/homeserver/constants"
+	"com/bbinsurance/log"
 	"net/http"
 )
 
 func main() {
+	// 初始化Logger
+	log.InitLogging("bb-insurance.log")
+	log.Info("Home Server Start")
+	// 初始化变量
+	constants.InitConstants()
 	//初始Http路径监控
 	http.Handle("/", http.FileServer(http.Dir(constants.STATIC_FOLDER)))
 	http.ListenAndServe(":"+constants.PORT, nil)
