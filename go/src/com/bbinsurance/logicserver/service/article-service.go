40dfe93ced389bb60ac8181b11dbc0db5037446f
jiaminchen <jmchen.ggm@gmail.com>
2018-01-20 22:19:35 +0800

    fix bug

--- go/src/com/bbinsurance/logicserver/service/article-service.go
+++ go/src/com/bbinsurance/logicserver/service/article-service.go
@@ -3,9 +3,13 @@ package service
 import (
 	"com/bbinsurance/logicserver/database"
 	"com/bbinsurance/logicserver/protocol"
+	"com/bbinsurance/webcommon"
 )
 
 func GetListArticle(startIndex int, length int) []protocol.Article {
 	articleList := database.GetListArticle(startIndex, length)
+	for i := 0; i < len(articleList); i++ {
+		articleList[i].ThumbUrl = webcommon.GenerateImgFileServerUrl(articleList[i].ThumbUrl)
+	}
 	return articleList
 }
